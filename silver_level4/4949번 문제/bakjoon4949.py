1228742ccb3b5e912ad3585956f892bb8708bb31
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-08-24 02:10:49 +0900

    bakjoon 4949

---
+++ silver_level4/4949번 문제/bakjoon4949.py
@@ -0,0 +1,20 @@
+import sys
+lines = sys.stdin.readlines()
+for line in lines[:-1]: #빈칸공백 제거X-> 마지막이 빈칸 제외하고 문장 전체를 의미
+    stack = []
+    for t in line:
+        if t in '([':
+            stack.append(t)
+        elif t == "]":
+            if not stack or stack.pop() != '[':
+                print('no')
+                break
+        elif t == ')':
+            if not stack or stack.pop() != '(':
+                print('no')
+                break
+        elif t == '.':
+            if stack: #.하나만 있지 않은 경우 -> false
+                print('no')
+            else:
+                print("yes")
