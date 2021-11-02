29bce2032062622eed97719ff037e3b6b6446b09
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-11-02 11:17:46 +0900

    이코테 정렬 두 배열의 원소 교체

---
+++ 알고리즘 유형/정렬/이코테 두 배열의 원소 교체.py
@@ -0,0 +1,27 @@
+import sys
+input=sys.stdin.readline
+
+#첫번째 줄에 N,K가 공백으로 구분되어 입력
+N,K=map(int,input().split())
+
+#배열 A의 원소들이 공백으로 입력
+A=list(map(int,input().split()))
+A.sort() #바꿔치기 위해 순서대로 작-> 큰 순으로 배열
+
+
+#배열 B의 원소들이 공백으로 입력
+B=list(map(int,input().split()))
+B=sorted(B,reverse=True) #바꿔치기 위해 역순으로 큰-> 작 순으로 배열
+
+#A의 원소들이 최대가 되도록 배열
+for i in range(K):
+    if A[i] < B[i]:
+        A[i], B[i]=B[i],A[i]
+    else:
+        break
+
+#A의 원소 합 출력
+print(sum(A))
+
+
+        
