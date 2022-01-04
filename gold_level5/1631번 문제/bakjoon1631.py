525a720008feb07124be46ecb847621753c1c538
zhenying2 <khappy9888@sookmyung.ac.kr>
2022-01-04 21:43:22 +0900

    bakjoon 1631.. 고민중..

--- gold_level5/1631번 문제/bakjoon1631.py
+++ gold_level5/1631번 문제/bakjoon1631.py
@@ -1,5 +1,5 @@
 #bakjoon 1631
-#2022.01.03
+#2022.01.03, 01.04
 
 #디스크 개수 N과 움직이는 횟수 M
 N,M=map(int,input().split())
@@ -12,20 +12,46 @@ for i in range(N):
 
 
 count=0 #움직이는 횟수 세는 변수
-while (count != M):
-        f(N,"A",list_result[N-count])
-    
-    
-
-def f(n, a, b):
+#최종 결과 저장 리스트    
+final_result=[]
+for _ in range(N):
+    final_result.append("A")
+print(final_result)  
+def f(n, a, b, c):
   if(n == 1):
+    global count
+    global final_result
+    #final_result[count]=c         <- 이부분 수정 필요.....
     count+=1 #count 수 증가
+    print(final_result)
+    print(count)
+    print(n)
   else:
-    f(n-1, "A","B")
-    f(1, "A", "C")
-    f(n-1, "B", "C")
+    f(n-1, a,c,b)
+    f(1, a, b,c)
+    f(n-1, b,a,c)
+    
+while (count != M):
+    if (list_result[N-1-count] == "B"):
+        f(N,"A","C","B")
+    elif (list_result[N-1-count] == "C"):
+        f(N,"A","B","C")
+    else:
+        copy_N=N
+        while (list_result[N-1-count] !="A"):
+            copy_N-=1
+        if (list_result[copy_N-1-count] =="B"):
+            f(copy_N,"A","C","B")
+        elif (list_result[N-1-count] == "C"):
+            f(copy_N,"A","B","C")
+        
+        
 
-n = int(input())
-print(2**n-1)
-if(n <= 20):
-  f(n, 1, 2, 3)
+#결과 출력        
+if (M == 0 ):
+    print("A"*N)
+else:
+    for i in range(N):
+        print(final_result[i],end="")
+
+
